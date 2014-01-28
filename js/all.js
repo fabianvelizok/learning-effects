63969d8e8480e2bd56568568116290c65091a209
FabianVeliz <velizfabianhoracio>
2014-01-28 12:45:16 -0300

    Fix Code

--- js/all.js
+++ js/all.js
@@ -2,8 +2,8 @@
 //En  pixeles
 $(document).on('ready',function(){
 
-	
-	//Guardo en el array el width de cada box 
+
+	//Guardo en el array el width de cada box
 	var arreglo = [];
 	$('.father div').each(function(){
     	arreglo.push($(this).width());
@@ -13,10 +13,10 @@ $(document).on('ready',function(){
     console.log(arreglo);
 
 	$('.box').css('width','0');
-	
+
 	$(window).scroll(function(){
-		if($('.box').scrollTop>$('.box')){		
-			
+		if($('.box').scrollTop>$('.box')){
+
 			//Variables
 			var i=0;
 			var l = arreglo.length;
@@ -24,43 +24,51 @@ $(document).on('ready',function(){
 			//Recorro el array
 			for(i=0;i<l;i++){
 				$('.box').eq(i).css('width',(arreglo[i]+'px'));
-			}			
+			}
 				console.log(arreglo[i]+'px');
 
-		}	
+		}
 	});
 });
 */
 
+var cargado=false;
 $(document).on('ready',function(){
+//console.log('width:20%;width:40%'.replace(/width\:(.*)\%/g,'$1'));
 
-	
-	//Guardo en el array el width de cada box 
+	//Guardo en el array el width de cada box
 	var arreglo = [];
-	$('.father div').each(function(){
+	$('.box').each(function(){
     	arreglo.push($(this).attr('style').replace('width:',''));
 	});
 
 	// Reseteo width
-	$('.box').css('width','0');
-	
+	$('.box').removeAttr('style');
+
+	setTimeout(function(){
+		$('.box').css({'transition':'width 3s ease-in-out'});
+		$(window).trigger('scroll');
+	},500);
+
+
 	$(window).scroll(function(){
 
 		var position=$(window).scrollTop();
 		//console.log(position);
 
-		var algo =$('.box').scrollTop();
+		var algo =$('.box').offset().top-$(window).height()/2;
 		//console.log(algo);
 
-		if(position>position){	
-
+		if((position>=algo || $(window).height()>algo) && !cargado){
+			cargado=true;
+			console.log('ejecutado');
 			var i=0;
 			var l = arreglo.length;
 
 			//Recorro el array
 			for(i=0;i<l;i++){
 				$('.box').eq(i).css('width',arreglo[i]);
-			}			
+			}
 
 		}//Fin if
 
